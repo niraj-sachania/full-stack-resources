55a99836f48fdf240e8b6892945c42da4383ebc1
niraj-sachania <>
2026-01-22 16:12:51 +0000

    feat: integrate django_summernote for enhanced content management in admin

--- fullstackresources/admin.py
+++ fullstackresources/admin.py
@@ -1,5 +1,17 @@
 from django.contrib import admin
+from django_summernote.admin import SummernoteModelAdmin
 from .models import Resource
+
+
+@admin.register(Resource)
+class PostAdmin(SummernoteModelAdmin):
+
+    list_display = ('title', 'slug', 'approved', 'created_at', 'username')
+    search_fields = ['title', 'description']
+    list_filter = ('approved', 'created_on')
+    prepopulated_fields = {'slug': ('title',)}
+    summernote_fields = ('description',)
+
 
 # Register your models here.
 admin.site.register(Resource)
