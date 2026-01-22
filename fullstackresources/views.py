6f46e5977bb8bb738fcad80669db470587ef3bcd
niraj-sachania <>
2026-01-22 17:55:33 +0000

    feat: add ResourceList view for displaying approved resources

--- fullstackresources/views.py
+++ fullstackresources/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Resource
 
 # Create your views here.
+
+
+class ResourceList(generic.ListView):
+    queryset = Resource.objects.filter(approved=1).order_by("created_at")
+    template_name = "index.html"
+    paginate_by = 6
