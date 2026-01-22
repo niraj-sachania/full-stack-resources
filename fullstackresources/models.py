df975a3b68bb7bd589974b2030ecd3dc1b18b327
niraj-sachania <>
2026-01-22 15:31:36 +0000

    feat: add Resource model

--- fullstackresources/models.py
+++ fullstackresources/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Resource(models.Model):
+    title = models.CharField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique=True)
+    username = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="resources")
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.title} | by {self.username}"
