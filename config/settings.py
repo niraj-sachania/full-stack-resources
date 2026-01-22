8bc5d4d023e63c30dba213bf3af6370ac8c9a796
niraj-sachania <>
2026-01-22 04:31:14 +0000

    chore: add a secure secret key

--- config/settings.py
+++ config/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--=+$*$p)c_(qul^ve7cbod*fnff^-)k353$s4l)67zdgz6*f6&'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
